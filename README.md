0615d2b5d629fabcb075b09eb38e3e25a82e7d08
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-05-27 17:33:21 +0530

    written test cases and it is working fine

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 ## Overview, Purpose & Features
 - it is a simple app which can be used to learn about testing in android.
 
-[Writing Our First Unit Tests - Testing on Android - Part 3](https://youtu.be/W0ag98EDhGc?t=830 "Named link title")
+[Writing Our First Unit Tests - Testing on Android - Part 3](https://youtu.be/PsoLeJOh30o "Named link title")
--- app/src/test/java/com/example/test_practice/RegistrationUtilTest.kt
+++ app/src/test/java/com/example/test_practice/RegistrationUtilTest.kt
@@ -15,4 +15,54 @@ class RegistrationUtilTest {
         )
         assertThat(result).isFalse()
     }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Abhinesh",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirmed password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Abhinesh",
+            "123456",
+            "abcdefg"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Abhinesh",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `less than 2 digit password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Abhinesh",
+            "abcd",
+            "abcd"
+        )
+        assertThat(result).isFalse()
+    }
 }
